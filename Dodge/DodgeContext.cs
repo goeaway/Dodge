a92350903397e291bb2d0bc0196bb5354b05d576
Joe Thompson-murdoch <joseph.thompson.murdoch@gmail.com>
2019-03-13 20:39:33 +0000

    added some items to the menu

--- Dodge/DodgeContext.cs
+++ Dodge/DodgeContext.cs
@@ -13,41 +13,60 @@ namespace Dodge
     {
         private IContainer _components;
         private NotifyIcon _notifyIcon;
+        private readonly DodgeState _state;
 
         public DodgeContext()
         {
             InitialiseContext();
+            _state = new DodgeState();
         }
 
         private void InitialiseContext()
         {
             _components = new Container();
+
+            var menu = new ContextMenuStrip();
+            var exitItem = new ToolStripMenuItem() {Text = "Exit", Name = "exitItem"};
+            var stopDodgingItem= new ToolStripMenuItem() { Text = "Stop Dodging", Name = "stopDItem"};
+            var startDodgingItem = new ToolStripMenuItem() { Text = "Start Dodging", Name = "startDItem"};
+            
+            exitItem.Click += (_, e) => QuitApplication();
+            stopDodgingItem.Click += StopDodgingItemOnClick; 
+            startDodgingItem.Click += StartDodgingItemOnClick;
+
+            menu.Items.AddRange(new ToolStripItem[] { startDodgingItem, stopDodgingItem, exitItem });
+
             _notifyIcon = new NotifyIcon(_components)
             {
-                ContextMenuStrip = new ContextMenuStrip(),
+                ContextMenuStrip = menu,
                 Icon = new Icon(@"C:\Users\Joe\source\repos\Dodge\Dodge\favicon.ico"),
                 Text = "Dodge",
                 Visible = true,
             };
 
             _notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
-            _notifyIcon.DoubleClick += NotifyIconOnDoubleClick;
-            _notifyIcon.MouseUp += NotifyIconOnMouseUp;
         }
 
-        private void NotifyIconOnMouseUp(object sender, MouseEventArgs e)
+        private void StartDodgingItemOnClick(object sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
 
-        private void NotifyIconOnDoubleClick(object sender, EventArgs e)
+        private void StopDodgingItemOnClick(object sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
 
         private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
         {
-            throw new NotImplementedException();
+            // if state.TryingToDodge we need to enable the stop dodging item
+            // vice versa
+        }
+
+        private void QuitApplication()
+        {
+            _notifyIcon.Visible = false;
+            Application.Exit();
         }
     }
 }
---
+++ Dodge/DodgeState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodge
+{
+    public class DodgeState
+    {
+        /// <summary>
+        /// Gets the PID of the process the app is trying to keep in view
+        /// </summary>
+        public int DodgingProcess { get; } = -1;
+
+        /// <summary>
+        /// Gets if the application is currently trying to keep a process in view
+        /// </summary>
+        public bool TryingToDodge => DodgingProcess != -1;
+    }
+}
